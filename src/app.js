5a8407a087ad0f4df91137acfe587efd4db1b1aa
Muhammad Bilal <m.bilalferoz1@gmail.com>
2026-09-10 18:08:50 +0500

    app

--- src/app.js
+++ src/app.js
@@ -1,8 +1,11 @@
 const express = require('express')
+const multer = require('multer')
 const postModel = require('./models/post.model')
 
 const app = express()
 app.use(express.json())
+
+const upload =multer({storage: multer.memoryStorage()})
 
 
 app.post('/create-post',async (req,res)=>{
