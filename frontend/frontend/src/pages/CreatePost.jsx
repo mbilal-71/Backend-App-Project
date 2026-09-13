9ce999f69c624aaeb91f817362f5075d780454d3
Muhammad Bilal <m.bilalferoz1@gmail.com>
2026-09-14 00:39:10 +0500

    react

--- frontend/frontend/src/pages/CreatePost.jsx
+++ frontend/frontend/src/pages/CreatePost.jsx
@@ -1,12 +1,25 @@
 import React from 'react'
+import axios from 'axios'
+import { useNavigate } from 'react-router-dom'
 
 const CreatePost = () => {
+
+    const navigate = useNavigate()
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        const formData = new FormData(e.target)
+        const response = await axios.post("http://localhost:3000/create-post", formData)
+        console.log(response.data)
+        navigate("/feed")
+    }
+
     return (
 
         <section className='create-post-section' >
 
             <h2>Create Post</h2>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <input type="file" name='image' accept='image/*' />
                 <input type='text' name="caption" placeholder='Write a caption...' required />
                 <button type='submit'>Post</button>
